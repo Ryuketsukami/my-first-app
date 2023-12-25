24034b77b03e9a5e63587ac8c03595153f5bb6e4
Ryuketsukami <Ryuketsukami@gmail.com>
2023-12-25 09:34:47 +0200

    there was an error with 4k screens

--- src/components/articles-cluster.js
+++ src/components/articles-cluster.js
@@ -8,15 +8,15 @@ export function ArticlesCluster(props) {
     return (
         <div className="lg:pr-56 lg:pl-56 pt-16 md:pr-8 md:pl-8 pl-4 pr-4">
             <div className='items-center flex flex-col'>
-            <div className="flex flex-row justify-between max-w-5xl w-full">
-                <h1 className="lg:text-xl md:text-sm text-xl font-bold text-gray-800 dark:text-gray-300">{section_name}</h1>
-                <Link to={`/categories/${section_meta_type}#`} className="bg-white dark:bg-neutral-700 dark:text-neutral-400
-                 dark:hover:bg-neutral-900 p-1.5 pr-2.5 pl-2.5 outline outline-1 outline-gray-200 rounded-md
-                    hover:bg-gray-800 hover:text-white hover:outline-gray-800
-                ">
-                    <p className="font-medium md:text-xs text-lg">View all</p>
-                </Link>
-            </div>
+                <div className="flex flex-row justify-between max-w-5xl w-full">
+                    <h1 className="lg:text-xl md:text-sm text-xl font-bold text-gray-800 dark:text-gray-300">{section_name}</h1>
+                    <Link to={`/categories/${section_meta_type}#`} className="bg-white dark:bg-neutral-700 dark:text-neutral-400
+                     dark:hover:bg-neutral-900 p-1.5 pr-2.5 pl-2.5 outline outline-1 outline-gray-200 rounded-md
+                        hover:bg-gray-800 hover:text-white hover:outline-gray-800
+                    ">
+                        <p className="font-medium md:text-xs text-lg">View all</p>
+                    </Link>
+                </div>
             </div>
             <div className="flex flex-col space-y-4 pt-8 pb-12 items-center">
                 {
